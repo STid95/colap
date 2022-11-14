707c1dd749c84bc862756bff80bea02880758914
ncontant <noemie.contant@symbol-it.fr>
2022-11-14 10:40:35 +0100

    foldable details

--- lib/screens/lists/components/name_preview.dart
+++ lib/screens/lists/components/name_preview.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:provider/provider.dart';
@@ -25,74 +27,125 @@ class NamePreview extends StatefulWidget {
   State<NamePreview> createState() => _NamePreviewState();
 }
 
-class _NamePreviewState extends State<NamePreview> {
+class _NamePreviewState extends State<NamePreview>
+    with SingleTickerProviderStateMixin {
   bool details = false;
+  late final AnimationController _controller;
+  late final CurvedAnimation animation;
+  final Duration duration = const Duration(milliseconds: 800);
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: duration);
+    _controller.addListener(() {
+      setState(() {});
+    });
+    animation = CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(
+        1 / 3,
+        1 / 3 * 1.5,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggleAnimation() {
+    if (_controller.status == AnimationStatus.completed) {
+      _controller.reverse();
+    } else {
+      _controller.forward();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 15),
-      child: GestureDetector(
-        child: details
-            ? Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      StreamProvider<ColapName>.value(
-                        value: Provider.of<DatabaseNameListService>(context)
-                            .getName(widget.listId, widget.nameId),
-                        initialData: ColapName(name: ''),
-                        builder: (context, child) {
-                          ColapName name = Provider.of<ColapName>(context);
-                          return NameDetails(
-                            name: name,
-                            onDelete: () => setState(() {
-                              details = false;
-                            }),
-                          );
-                        },
-                      )
-                    ],
+      child: Column(children: [
+        AnimatedBuilder(
+          animation: animation,
+          builder: (context, child) => Visibility(
+            visible: animation.value >= 0.5,
+            child: Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001)
+                ..rotateX(pi + pi * animation.value),
+              alignment: FractionalOffset.topCenter,
+              child: GestureDetector(
+                onTap: _toggleAnimation,
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        StreamProvider<ColapName>.value(
+                          value: Provider.of<DatabaseNameListService>(context)
+                              .getName(widget.listId, widget.nameId),
+                          initialData: ColapName(name: ''),
+                          builder: (context, child) {
+                            ColapName name = Provider.of<ColapName>(context);
+                            return NameDetails(
+                              name: name,
+                              onDelete: () => setState(() {
+                                details = false;
+                              }),
+                            );
+                          },
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              )
-            : ListTile(
-                contentPadding: EdgeInsets.zero,
-                horizontalTitleGap: 0,
-                title: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.7,
-                  child: Text(
-                    widget.name,
-                    style: Theme.of(context).textTheme.headline5,
-                    overflow: TextOverflow.clip,
-                  ),
-                ),
-                leading: InkWell(
-                    child: Icon(Icons.delete,
-                        size: 20, color: Theme.of(context).colorScheme.primary),
-                    onTap: () => showRemoveDialog(context, (() {
-                          setState(() {
-                            details = false;
-                          });
-                          ColapName(name: widget.name, uid: widget.nameId)
-                              .remove(widget.listId);
-                        }))),
-                trailing: Hero(
-                  tag: widget.nameId,
-                  child: RatingBarIndicator(
-                      itemSize: 30,
-                      itemBuilder: (context, _) => Icon(Icons.star,
-                          color: Theme.of(context).colorScheme.primary),
-                      itemCount: 5,
-                      rating: widget.averageGrade.toDouble()),
                 ),
               ),
-        onTap: () => setState(() {
-          details = !details;
-        }),
-      ),
+            ),
+          ),
+        ),
+        Visibility(
+          visible: animation.value < 0.5,
+          child: ListTile(
+            onTap: _toggleAnimation,
+            contentPadding: EdgeInsets.zero,
+            horizontalTitleGap: 0,
+            title: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: Text(
+                widget.name,
+                style: Theme.of(context).textTheme.headline5,
+                overflow: TextOverflow.clip,
+              ),
+            ),
+            leading: InkWell(
+                child: Icon(Icons.delete,
+                    size: 20, color: Theme.of(context).colorScheme.primary),
+                onTap: () => showRemoveDialog(context, (() {
+                      setState(() {
+                        details = false;
+                      });
+                      ColapName(name: widget.name, uid: widget.nameId)
+                          .remove(widget.listId);
+                    }))),
+            trailing: Hero(
+              tag: widget.nameId,
+              child: RatingBarIndicator(
+                  itemSize: 30,
+                  itemBuilder: (context, _) => Icon(Icons.star,
+                      color: Theme.of(context).colorScheme.primary),
+                  itemCount: 5,
+                  rating: widget.averageGrade.toDouble()),
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
